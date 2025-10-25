de2b4cbccb51295716291341d28693061636879d
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-10-25 18:10:28 +0100

    Add Exceptions

---
+++ src/exeption/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package exeption;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}

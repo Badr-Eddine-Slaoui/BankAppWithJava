272d2c055eca0627bdce06bd48aefa8fdc7b1b38
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-10-26 16:41:41 +0100

    Add Controllers Logic

--- src/controller/AccountController.java
+++ src/controller/AccountController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import command.*;
+import dto.CurrentAccountDTO;
+import dto.SavingAccountDTO;
 import service.BankService;
 
 public class AccountController extends Controller{
@@ -9,23 +11,29 @@ public class AccountController extends Controller{
         super(bankService);
     }
 
-    public void createCurrentAccount(){
-        new CreateCurrentAccountCommand(bankService).execute();
+    public void createCurrentAccount(String name, double balance){
+        CurrentAccountDTO accountDTO = new CurrentAccountDTO();
+        accountDTO.setUserName(name);
+        accountDTO.setBalance(balance);
+        new CreateCurrentAccountCommand(bankService, accountDTO).execute();
     }
 
-    public void createSavingAccount(){
-        new CreateSavingAccountCommand(bankService).execute();
+    public void createSavingAccount(String name, double balance){
+        SavingAccountDTO accountDTO = new SavingAccountDTO();
+        accountDTO.setUserName(name);
+        accountDTO.setBalance(balance);
+        new CreateSavingAccountCommand(bankService, accountDTO).execute();
     }
 
-    public void displayBalance(){
-        new DisplayBalanceCommand(bankService).execute();
+    public void displayBalance(String code){
+        new DisplayBalanceCommand(bankService, code).execute();
     }
 
-    public void displayOperation(){
-        new DisplayOperationsCommand(bankService).execute();
+    public void displayOperations(String code){
+        new DisplayOperationsCommand(bankService, code).execute();
     }
 
-    public void searchAccount(){
-        new SearchAccountCommand(bankService).execute();
+    public void displayAccount(String code){
+        new DisplayAccountCommand(bankService, code).execute();
     }
 }

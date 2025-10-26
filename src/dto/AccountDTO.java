4a63c9e12571098deb5fa6a4e8e2e062727e90f8
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-10-26 20:23:58 +0100

    Fix AccountDTO

--- src/dto/AccountDTO.java
+++ src/dto/AccountDTO.java
@@ -1,8 +1,14 @@
 package dto;
 
+import entity.Operation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AccountDTO {
     private String userName;
     private double balance;
+    private List<OperationDTO> operationsList = new ArrayList<>();
 
     public String getUserName(){
         return userName;
@@ -19,4 +25,26 @@ public abstract class AccountDTO {
     public void setBalance(double balance){
         this.balance = balance;
     }
+
+    public List<OperationDTO> getOperationsList(){
+        return operationsList;
+    }
+
+    public void setOperationsList(List<OperationDTO> operationsList){
+        this.operationsList = operationsList;
+    }
+
+    public void displayDetails(){
+        System.out.println("========================================");
+        System.out.printf("| %-12s | %s%n", "Name", getUserName());
+        if (this instanceof CurrentAccountDTO) {
+            System.out.printf("| %-12s | Current Account%n", "Type");
+            System.out.printf("| %-12s | %.2f DH%n", "overdraft", ((CurrentAccountDTO)this).getOverdraft());
+        } else if (this instanceof SavingAccountDTO) {
+            System.out.printf("| %-12s | Saving Account%n", "Type");
+            System.out.printf("| %-12s | %.2f %% %n", "interest rate", ((SavingAccountDTO)this).getInterestRate() * 100);
+        }
+        System.out.printf("| %-12s | %.2f DH%n", "Balance", getBalance());
+        System.out.println("========================================");
+    };
 }
